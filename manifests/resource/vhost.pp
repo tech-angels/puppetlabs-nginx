1f455138613393f4a71ceafef7bbc1848420e7b4
pradermecker <pradermecker@cirb.irisnet.be>
2012-04-24 13:55:38 +0200

    Implement options for the server block.

--- manifests/resource/vhost.pp
+++ manifests/resource/vhost.pp
@@ -12,6 +12,7 @@
 #   [*ipv6_listen_port*] - Default IPv6 Port for NGINX to listen with this vHost on. Defaults to TCP 80
 #   [*index_files*]      - Default index files for NGINX to read when traversing a directory
 #   [*access_log_name*]  - File name (without path) to the access log file. Default to server name
+#   [*option*]           - List of option rules.
 #   [*proxy*]            - Proxy server(s) for the root location to connect to.  Accepts a single value, can be used in
 #                          conjunction with nginx::resource::upstream
 #   [*proxy_read_timeout*] - Override the default the proxy read timeout value of 90 seconds
@@ -43,11 +44,14 @@ define nginx::resource::vhost(
   $ssl_cert           = undef,
   $ssl_key            = undef,
   $access_log_name    = $name,
+  $options            = [],
   $proxy              = undef,
   $proxy_read_timeout = $nginx::params::nx_proxy_read_timeout,
   $index_files        = ['index.html', 'index.htm', 'index.php'],
   $www_root           = undef
 ) {
+
+  validate_array($options)
 
   File {
     owner => 'root',
